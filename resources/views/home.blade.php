b30fb0359e822c8a4b81b53ed0fec3fb758a2aea
Jozaguts <jozaguts@hotmail.com>
2019-02-17 14:18:25 -0600

    nav home principal terminado

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,29 +1,49 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/app.css">
+<link rel="stylesheet" href="{{asset('css/login-resposive.css')}}">
+    <title>Inico</title>
+</head>
+<body>
+    <nav class="nav navbar bg-success">
+        <div class="nav-title-container">
+            <h2 class="nav-title-container__title">Tropical-Adventures</h2>
+        </div>
+        <div class="user-name-contanier">
+               
+        <a href="#" class="user-name-contanier__link">{{Auth::user()->name}}</a>
+        <span class="arrow-down"></span> 
+            
+        </div>
+    </nav>
+            
+{{-- <div class="logout-contanier">
 
-@section('content')
-
-
-
-@auth
 <li class="nav-item dropdown">
-    <a id="navbarDropdown" class="nav-link dropdown-toggle text-success bg-dark btn-logout" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-        {{ Auth::user()->name }} <span class="caret"></span>
-    </a>
+<a id="navbarDropdown" class="nav-link dropdown-toggle text-success bg-dark btn-logout" href="#" role="button"                  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+{{ Auth::user()->name }} <span class="caret"></span>
+</a>
+<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+<a class="dropdown-item" href="{{ route('logout') }}"
+onclick="event.preventDefault();
+document.getElementById('logout-form').submit();">
+{{ __('Cerrar sesión') }}
+</a>
+<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+@csrf
+</form>
+</div>
+</li>
 
-    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-        <a class="dropdown-item" href="{{ route('logout') }}"
-           onclick="event.preventDefault();
-                         document.getElementById('logout-form').submit();">
-            {{ __('Cerrar sesión') }}
-        </a>
-
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-            @csrf
-        </form>
-    </div>
-</li>  
+</div> --}}
+       
   
 
+@auth
 <ul class="nav justify-content-center">
     <li class="nav-item bg-light col-sm-6 " >
         <a class="nav-link text-dark font-weight-bold custom-link" href="#">Reservaciones</a>
@@ -51,6 +71,9 @@
     </li>  
   
 </ul>
-
 @endauth
-@endsection
+
+
+    
+</body>
+</html>
