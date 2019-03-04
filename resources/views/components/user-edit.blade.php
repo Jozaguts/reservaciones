dd7c667477808c9d68f216f91e01c0358bed84c0
Jozaguts <jozaguts@hotmail.com>
2019-03-04 12:14:44 -0600

    actualizar usuarios

--- resources/views/components/user-edit.blade.php
+++ resources/views/components/user-edit.blade.php
@@ -1,24 +1,24 @@
 
 <div class="container-fluid d-none" id="userEditModal">
-    <span class="close-modal" id="closeModalEdit"></span>
+    <span class="close-modal-edit" id="closeModalEdit"></span>
     <div class="row justify-content-center row-modal">
         <div class="col-md-6 modal-content">
             <div class="card">
                 <div class="header-modal-container">
-                    <div class="card-header" id="card-header">{{ __('Editar Usuario') }} </div>
+                    <div class="card-header" id="card-header">{{ __('Editar Usuario') }} <span class="text-capitalize font-weight-bold" id="userNametitle"> </span></div>
                     
                 </div>
                 <div class="card-body">
-                    <form method="PUT" action="{{route ('usuarios.update','test') }}">
-                        @csrf
+                    <form method="PUT" action="{{url ('usuarios', $user) }}">
+                         @csrf
                         @method('PUT')
-
+                        {{var_dump($user->id)}}
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Nombre de usuario') }}<br> <small id="helpId" class="text-muted">(Correo electrónico)</small>
                             </label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $user->email}}" required autofocus>
+                                <input id="editEmail" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="" autofocus >
                                 @if ($errors->has('email'))
                                     <span class="invalid-feedback" role="alert">
                                             <strong>{{ $errors->first('email') }}</strong>
@@ -31,7 +31,7 @@
                             </label>
 
                             <div class="col-md-6">
-                                <input id="first_name" type="text" class="form-control{{ $errors->has('first_name') ? ' is-invalid' : '' }}" name="first_name" value="{{ old('first_name') }}" required >
+                                <input id="editFirst_name" type="text" class="form-control{{ $errors->has('first_name') ? ' is-invalid' : '' }}" name="first_name" value="" >
 
                                 @if ($errors->has('first_name'))
                                     <span class="invalid-feedback" role="alert">
@@ -44,7 +44,7 @@
                             <label for="last_name" class="col-md-4 col-form-label text-md-right">{{ __('Apellidos') }}</label>
 
                             <div class="col-md-6">
-                                <input id="last_name" type="text" class="form-control{{ $errors->has('last_name') ? ' is-invalid' : '' }}" name="last_name" value="{{ old('last_name') }}" required >
+                                <input id="editLast_name" type="text" class="form-control{{ $errors->has('last_name') ? ' is-invalid' : '' }}" name="last_name" value="" >
 
                                 @if ($errors->has('last_name'))
                                     <span class="invalid-feedback" role="alert">
@@ -57,7 +57,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                <input id="editPassword" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" >
 
                                 @if ($errors->has('password'))
                                     <span class="invalid-feedback" role="alert">
@@ -67,17 +67,10 @@
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirma contraseña') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-                        <div class="form-group row">
                             <label for="role" class="col-md-4 col-form-label text-md-right">{{ __('Perfil') }}</label>
 
                             <div class="col-md-6">
-                                <select name="role" id="role" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }} text-capitalize" required autofocus>
+                                <select name="role" id="editRole" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }} text-capitalize" >
 
                                     @foreach ($roles as $key =>$value)
                                         <option  value="{{$value}}" class="text-capitalize">{{$value}}</option>
@@ -98,7 +91,7 @@
                             <label for="department" class="col-md-4 col-form-label text-md-right">{{ __('Departamento') }}</label>
 
                             <div class="col-md-6">
-                                <input id="department" type="text" class="form-control{{ $errors->has('department') ? ' is-invalid' : '' }}" name="department" value="{{ old('department') }}" required autofocus>
+                                <input id="editDepartment" type="text" class="form-control{{ $errors->has('department') ? ' is-invalid' : '' }}" name="department" value="" >
 
                                 @if ($errors->has('department'))
                                     <span class="invalid-feedback" role="alert">
@@ -111,7 +104,7 @@
                             <label for="active" class="col-md-4 col-form-label text-md-right">{{ __('Activo') }}</label>
 
                             <div class="col-md-6 ">
-                                <input id="active" type="checkbox" class="align-self-start form-control{{ $errors->has('active') ? ' is-invalid' : '' }}" name="active" value="{{ old('active') }}">
+                                <input id="editActive" type="checkbox" class="align-self-start form-control{{ $errors->has('active') ? ' is-invalid' : '' }}" name="active" value="">
 
                                 @if ($errors->has('active'))
                                     <span class="invalid-feedback" role="alert">
@@ -120,10 +113,11 @@
                                 @endif
                             </div>
                         </div>
+                        <input type="hidden" name="userId" id="userId" value="">
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4 main-nav">
-                                <button type="submit" class="btn main-navbar">
+                            <button type="submit" class="btn main-navbar" id="btnEdit">
                                     {{ __('Actualizar Usuario') }}
                                 </button>
                             </div>
