0832f16bf3b382d988a63a8e8d9bc855f5c193f7
Jozaguts <jozaguts@hotmail.com>
2019-04-12 13:11:39 -0500

    modal add actividades front listo

---
+++ database/migrations/2019_04_12_175243_create_anticipos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnticiposTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('anticipos', function (Blueprint $table) {
+            $table->string('nombre');
+            $table->integer('porcentaje');
+            $table->boolean('active');
+            $table->boolean('remove');        
+
+            $table->unsignedInteger('usuarios_id');
+            $table->foreign('usuarios_id')->references('id')->on('usuarios');	
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('anticipos');
+    }
+}
--- resources/js/activities.js
+++ resources/js/activities.js
@@ -9,3 +9,73 @@ showContent = function (e){
 elements .forEach(element => {
    element.classList.add('tr-bg');
 });
+
+const home_tab = document.getElementById('home_tab');
+showTab =()=>{
+
+}
+home_tab.addEventListener('click',()=>{
+  
+}) 
+
+//add actividades 
+
+
+let AddTipoActividadesForm = document.getElementById('AddTipoActividadesForm');
+
+AddTipoActividadesForm.addEventListener('submit',(e)=>{
+e.preventDefault();
+let datos = new FormData(AddTipoActividadesForm) 
+let clave = datos.get('clave')
+let tipounidad = datos.get('tipounidad')
+let nombre = datos.get('nombre')
+let color = datos.get('color')
+let token = $("input[name=_token]").val();
+let usuarios_id = datos.get('idusuario')
+let active;
+  if($('#active').is(':checked')){
+    active = 0;
+  }else{
+    active = 1;
+  }
+  let remove;
+  if($('#remove').is(':checked')){
+    remove = 0;
+  }else{
+    remove = 1;
+  }
+
+
+let route = 'tipoactividades'
+
+$.ajax({
+  url:route,
+  headers:{'X-CSRF-TOKEN':token},
+  type:'POST',
+  dataType: 'json',
+  data: {clave: clave, nombre: nombre, color: color, usuarios_id: usuarios_id, active: active, remove: remove,tipounidad_id: tipounidad},
+  success: function (data) {
+  
+    console.log(data);
+    if(data.error == 'true'){
+      $('#errorsIntoModal').html(data.errors);
+        $('#message-errorIntoModal').fadeIn();
+        setTimeout(() => {
+          $('#message-error').fadeOut();
+        }, 3000);
+        // setTimeout("location.reload(true);",3000)
+        
+    }else{
+      $('#successIntoModal').html(data.ok);
+        $('#message-successIntoModal').fadeIn();
+        setTimeout(() => {
+          $('#message-successIntoModal').fadeOut();
+        }, 3000);
+        setTimeout("location.reload(true);",3000)
+    }
+  }
+  
+
+})
+
+})
