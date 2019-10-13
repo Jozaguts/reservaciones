f9e6dd1d7910844b69bb93308b94c871e0e05d05
Jozaguts <jozaguts@hotmail.com>
2019-10-12 19:12:29 -0500

    asgin

--- app/Http/Controllers/AsignacionesController.php
+++ app/Http/Controllers/AsignacionesController.php
@@ -21,14 +21,14 @@ class AsignacionesController extends Controller
 
    public function getAsignaciones(Request $request){
 
-      // $unidades = EquiposYUnidades::select(['id','clave','active','capacidad']);
-      $uni = DB::table('unidades as un')
-      ->join('tipounidades as tun', 'un.idtipounidad', '=', 'tun.id')
-      ->select('un.id','un.clave', 'un.descripcion as Unidad', 'tun.id as tunid','tun.nombre as tipo_unidad', 'un.capacidad', DB::raw('IF(un.active=1, "Activo", "Desactivo") as status'))
-      ->where([['un.remove', '=', '0']])
-      ->get();
+      $unidades = EquiposYUnidades::select(['id','clave','active','capacidad']);
+      // $uni = DB::table('unidades as un')
+      // ->join('tipounidades as tun', 'un.idtipounidad', '=', 'tun.id')
+      // ->select('un.id','un.clave', 'un.descripcion as Unidad', 'tun.id as tunid','tun.nombre as tipo_unidad', 'un.capacidad', DB::raw('IF(un.active=1, "Activo", "Desactivo") as status'))
+      // ->where([['un.remove', '=', '0']])
+      // ->get();
       // Datatables::eloquent($model)->make(true);
-      return Datatables::of($uni)->make(true);
+      return Datatables::of($unidades)->make(true);
       // return Datatables::of( EquiposYUnidades::select(['id','descripcion','clave','active','capacidad']))
       
 
