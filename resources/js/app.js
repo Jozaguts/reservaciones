c05371b6fac506d73a182304cbe5d31f284f9a24
Sagit Gutiérrez <jozaguts@hotmail.com>
2020-03-12 12:59:24 -0600

    add select sercheable

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 require('./bootstrap');
-
+import 'vue-search-select/dist/VueSearchSelect.css'
 window.moment = require('moment');
 
 import swal from 'sweetalert';
---
+++ resources/js/components/SelectDinamico.vue
@@ -0,0 +1,60 @@
+<template>
+    <model-select :options="options"
+                  v-model="item"
+                  placeholder="Seleccione un comisionista">
+    </model-select>
+</template>
+
+<script>
+    import { ModelSelect } from 'vue-search-select';
+
+    export default {
+        name: "SelectDinamico",
+        data () {
+            return {
+                options: [
+                    { value: '1', text: 'COM01 Alfredo Rios'},
+                    { value: '2', text: 'COM02 Jose Gutíerrez' },
+                    { value: '3', text: 'COM03 David Hernandez' },
+                    { value: '4', text: 'COM04 Fernanda Suarez' },
+                    { value: '5', text: 'COM05 Ernesto Valle' }
+                ],
+                item: {
+                    value: '',
+                    text: ''
+                },
+                options2: [
+                    { value: '1', text: 'aa' + ' - ' + '1' },
+                    { value: '2', text: 'ab' + ' - ' + '2' },
+                    { value: '3', text: 'bc' + ' - ' + '3' },
+                    { value: '4', text: 'cd' + ' - ' + '4' },
+                    { value: '5', text: 'de' + ' - ' + '5' }
+                ],
+                item2: ''
+            }
+        },
+        methods: {
+            reset () {
+                this.item = {}
+            },
+            selectFromParentComponent1 () {
+                // select option from parent component
+                this.item = this.options[0]
+            },
+            reset2 () {
+                this.item2 = ''
+            },
+            selectFromParentComponent2 () {
+                // select option from parent component
+                this.item2 = this.options2[0].value
+            }
+        },
+        components: {
+            ModelSelect
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
